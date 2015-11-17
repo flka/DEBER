b4db45b83ae550735038b64bd01587e705eacc1a
William280793 <william.andradem@ug.edu.ec>
2015-11-17 01:16:01 +0100

    Update Lalomiamor.java

--- lalomiamor/src/lalomiamor/Lalomiamor.java
+++ lalomiamor/src/lalomiamor/Lalomiamor.java
@@ -15,7 +15,7 @@ public class Lalomiamor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLA LALO MI AMOR");
+        System.out.println("HOLA LALO MI AMOR DE MI VIDA");
     }
     
 }
